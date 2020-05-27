b5e72a191322e8867e74d5031428b24eee5e4b85
chend0316 <chendong0316@163.com>
2020-05-27 09:17:25 +0800

    配置文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,28 +17,43 @@ module.exports = {
         ]
       },
       {
-        title: '算法',
+        title: '力扣算法刷题',
         collapsable: false,
         children: [
           {
-            title: '专题讲解',
+            title: '算法专题',
             children: [
               { title: '最优连续子串', path: '/algorithm/substr' },
               { title: '二分法查找', path: '/algorithm/binsearch' },
+              { title: '滑动窗口法', path: '/algorithm/sliding-window' },
             ]
-          }
+          },
+          { title: '语言差异', path: '/algorithm/language' }
         ]
       },
       {
         title: '前端',
         collapsable: false,
         children: [
+          { title: '发展史', path: '/frontend/history/' },
           {
             title: 'Vue',
             collapsable: false,
             // path: '/frontend/vue/',
             children: [
               { title: '从零开始搭建Vue环境', path: '/frontend/vue/vue-setup/' },
+              {
+                title: 'Vue代码测试',
+                children: [
+                  { title: '对Vue组件进行单元测试', path: '/frontend/vue/unit-test/' },
+                ]
+              },
+            ]
+          },
+          {
+            title: 'Electron桌面端开发', path: '/frontend/electron/',
+            collapsable: false,
+            children: [
             ]
           }
         ]
@@ -49,6 +64,20 @@ module.exports = {
         children: [
           { title: 'Cython入门教程', path: '/backend/cython/' },
           { title: 'Protobuf第三方扩展开发指南', path: '/backend/protobuf/' },
+        ]
+      },
+      {
+        title: '专业基础四大件',
+        children: [
+          { title: '数据结构与算法', path: '/cs/dsc/' },
+          { title: '操作系统', path: '/cs/os/' },
+          {
+            title: '计算机网络', path: '/cs/network/',
+            children: [
+              { title: '发展史', path: '/cs/network/history' }
+            ]
+          },
+          { title: '编译原理', path: '/cs/compile/' },
         ]
       }
     ]
